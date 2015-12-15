00a31c97f3a6bad628efefdbf6747fe3d88b76b9
root <root@rpi-sb-blanc.42.fr>
2015-12-15 14:20:48 +0100

    groupe 2

---
+++ groupe2/README.md
@@ -0,0 +1,5 @@
+# groupe 2
+
+le groupe des verins/servos qui bougent selon le volume sonore ambiant
+
+db.py sert d'exemple de mesure avec un micro USB sur Raspberry Pi 2
---
+++ groupe2/db.py
@@ -0,0 +1,58 @@
+"""PyAudio example: Record a few seconds of audio and save to a WAVE file."""
+
+from math import log10
+import pyaudio
+import wave
+import numpy
+import signal
+import sys
+
+CHUNK = 1024
+FORMAT = pyaudio.paUInt8
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+p = pyaudio.PyAudio()
+
+def get_db(data):
+    min = 128
+    max = 128
+    for i in xrange(len(data)):
+        c = ord(data[i])
+        if (c < min):
+            min = c
+        if (c > max):
+            max = c
+    db = int(20.0 * log10(max - min))
+    return db
+
+print("* get_device_count(): %d" % p.get_device_count())
+
+for i in xrange(p.get_device_count()):
+    print("* %d: %s" % (i, p.get_device_info_by_index(i)['name']));
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("* listening")
+
+def signal_handler(signal, frame):
+        print('You pressed Ctrl+C!')
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+#for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+while True:
+    data = stream.read(CHUNK)
+    #print "#" * get_db(str(data))
+    db = get_db(str(data))
+    print "%03d %s" % (db, "#" * db)
+
